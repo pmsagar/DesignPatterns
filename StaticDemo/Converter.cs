f429a4d4820d4448c5f6cbe1b85a61e6067aab58
Mani Sagar <mani.sagar@pinkerton.com>
2021-01-31 17:15:41 +0530

    Static demo

---
+++ StaticDemo/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDemo
+{
+    /// <summary>
+    /// Whenver there is a static computation we can go for static classes, but these are not as flexibles as Singleton
+    /// </summary>
+    public static class Converter
+    {
+        public static double ToFarenheit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+
+        public static double ToCelcius(double farenheit)
+        {
+            return (farenheit - 32) * 5/9;
+        }
+    }
+}
---
+++ StaticDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius = 37; double farenheit = 98.6;
+            Console.WriteLine("C to F:"+ Converter.ToFarenheit(celcius));
+            Console.WriteLine("F to C:"+ Converter.ToCelcius(farenheit));
+            Console.ReadLine();
+        }
+    }
+}
