9783ee7652eb73391f0542bda60599b27f56187a
Mani Sagar <mani.sagar@pinkerton.com>
2021-01-31 17:15:31 +0530

    Lazy and eager loading singleton

---
+++ LazyEagerSingletonDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LazyEagerSingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+    () => PrintStudentDetailsEagerSingleton(),
+    () => PrintEmployeeDetailsEagerSingleton()
+    );
+
+            Parallel.Invoke(
+() => PrintStudentDetailsLazySingleton(),
+() => PrintEmployeeDetailsLazySingleton()
+);
+        }
+
+
+        private static void PrintStudentDetailsEagerSingleton()
+        {
+            EagerSingleton student = EagerSingleton.GetInstance;
+            student.PrintDetails("This is student message.");
+        }
+
+        private static void PrintEmployeeDetailsEagerSingleton()
+        {
+            EagerSingleton employee = EagerSingleton.GetInstance;
+            employee.PrintDetails("This is employee message.");
+        }
+
+        private static void PrintStudentDetailsLazySingleton()
+        {
+            LazySingleton student = LazySingleton.GetInstance;
+            student.PrintDetails("This is student message.");
+        }
+
+        private static void PrintEmployeeDetailsLazySingleton()
+        {
+            LazySingleton employee = LazySingleton.GetInstance;
+            employee.PrintDetails("This is employee message.");
+        }
+    }
+
+}
---
+++ LazyEagerSingletonDemo/Singleton.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Lazy loading  - Improves performance; avoids unnecessary loads till the point object is accessed
+// Reduces memory footprint on start-up
+// Faster Application Load
+
+
+// Eager loading - preinstantiation of the object
+// Commonly used in lower memory footprint
+namespace LazyEagerSingletonDemo
+{
+    //3. Class is sealed to avoid instantiating an object by using nested classes/sub classes
+    public sealed class EagerSingleton
+    {
+        private static int counter = 0;
+        // To facilitate eager loading
+        private static readonly EagerSingleton instance = new EagerSingleton();
+        // 2. Defined a public property to access the single instance
+        public static EagerSingleton GetInstance
+        {
+            get 
+            {
+                return instance;
+            }
+        }
+
+        //1. Declared private to restrict direct instantiation
+        private EagerSingleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value: " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+
+    public sealed class LazySingleton
+    {
+        private static int counter = 0;
+        // To facilitate lazy loading
+        // By default lazy keyword is thread safe 
+        private static readonly Lazy<LazySingleton> instance = new Lazy<LazySingleton>(() => new LazySingleton());
+        // 2. Defined a public property to access the single instance
+        public static LazySingleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        //1. Declared private to restrict direct instantiation
+        private LazySingleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value: " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
